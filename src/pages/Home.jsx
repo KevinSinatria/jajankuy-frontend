e1f2a084cab2a105f999b4cbc5c184bafe0eb10e
waqqir humaid <waqqirhumaid1@gmail.com>
2025-06-18 12:57:43 +0700

    feat: penambahan component card

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Card from "../components/Card";
+
+export default function Home(){
+    return (
+        <>
+            <h1>Nyoba nyoba asikkk</h1>
+            <Card/>
+        </>
+    )
+}
