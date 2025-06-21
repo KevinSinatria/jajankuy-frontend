486815480a6ba3a53fea7d1ab4cc258cc67a2d3c
waqqir humaid <waqqirhumaid1@gmail.com>
2025-06-21 22:47:21 +0700

    feat:Footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
 
 export default function HomePage() {
 
@@ -8,6 +9,9 @@ export default function HomePage() {
       <main className='w-full min-h-screen flex flex-col bg-neutral-50 text-neutral-600'>
         <Navbar />
       </main>
+  
+      <Footer/> 
+
     </div>
   );
 }
