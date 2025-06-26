122557d5f28f242a49d087e1a1c5012e59db2a5c
waqqir humaid <waqqirhumaid1@gmail.com>
2025-06-26 21:10:35 +0700

    feat:LoginPageBaru

---
+++ src/components/PesananDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PesananDetail() {
+  return (
+    <div>PesananDetail</div>
+  )
+}
+
+export default PesananDetail
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,19 +4,33 @@ import Navbar from "../components/Navbar";
 
 export default function LoginPage() {
   return (
-  
-      <div className="bg-[#B4DC62] h-screen w-screen overflow-hidden ">
-        <Navbar /> 
+    // <div className="bg-[#B4DC62] h-screen w-screen overflow-hidden ">
+    //   <Navbar />
+    //   <div className="lg:flex justify-between relative items-center mx-auto">
+    //     <img src={Logo} alt="" className="mt-20 w-250 relative z-0 bottom-8"/>
+    //       <div className="relative md:absolute left-1/3">
+    //         <LoginForm />
+    //       </div>
+    //   </div>
+    // </div>
+    <div className="relative">
+      {/* Konten utama paling bawah */}
+      <div className="bg-[#B4DC62] h-screen absolute inset-0 z-[-20]"></div>
 
-    
-        <div className="lg:flex justify-between relative items-center mx-auto">
-          <img src={Logo} alt="" className="mt-20 w-200 relative z-0"/>
-          <div className="lg:relative z-10 left-1/6 lg:mx-0 flex">
-            <div className="lg:relative absolute z-10 left-1/6 lg:mx-0">
-              <LoginForm />
-            </div>
+      {/* Background Shapes di atas layer hijau */}
+      <div className="absolute inset-0 -z-10">
+        <img src={Logo} alt="" className="mt-20 w-250 relative z-0 bottom-8" />
+      </div>
+
+      {/* Konten lain di atas semuanya */}
+      <div className="relative z-10">
+        <Navbar />
+        <div className="flex justify-center md:justify-end md:mt-0 py-40 px-4 md:px-20">
+          <div className="w-full max-w-lg">
+            <LoginForm />
           </div>
         </div>
       </div>
+    </div>
   );
 }
