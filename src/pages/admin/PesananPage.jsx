c3c82c00f1d0ade3d45e8cadcd64d3cb6361a6e4
waqqir humaid <waqqirhumaid1@gmail.com>
2025-07-02 23:16:53 +0700

    feat:PesananAdmin Tabel

--- src/pages/admin/PesananPage.jsx
+++ src/pages/admin/PesananPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar'
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+import TabelPesanan from "../../components/TabelPesanan";
 
 const PesananPage = () => {
   return (
     <div className="flex min-h-screen bg-white">
       <Sidebar />
+      <div className="flex-1 p-7 space-y-6">
+        <div className="text-4xl font-bold text-center mb-9">
+          Lihat pesanan para Klien!
+        </div>
+        <TabelPesanan/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PesananPage
+export default PesananPage;
